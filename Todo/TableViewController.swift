144078dcf72c31ac592d5c88b2032cf169838acb
Rajasekhar <rajasekhar.alimili@devrabbit.com>
2019-08-06 14:24:03 +0530

    Added Tableview Delegates and Datasource methods

---
+++ Todo/TableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Apple on 06/08/19.
+//  Copyright © 2019 DevRabbit. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let itemsArr = ["Buy Apples", "Oranges", "Grapes"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        cell.textLabel?.text = itemsArr[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+
+
+}
+
